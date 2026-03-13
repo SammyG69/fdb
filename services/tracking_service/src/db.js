8a3473b0faf04f26fb5c614d45cfc263b433f6ad
SammyG69 <samarthgupta14@yahoo.com.sg>
2026-03-14 01:38:55 +1100

    Made some changes to the .env files and docker container configurations to be able to persist events onto database

--- services/tracking_service/src/db.js
+++ services/tracking_service/src/db.js
@@ -1,6 +1,7 @@
 import { Pool } from "pg";
 
+console.log("Here we are arent we")
+
 export const pool = new Pool ({
   connectionString: process.env.DATABASE_URL,
-  console.log(connectionString);
 });
