5fab1927d965c7ac840de9deacb79891403eb3df
SammyG69 <samarthgupta14@yahoo.com.sg>
2026-04-06 23:23:01 +1000

    Completing the attribute update event if the user changes their profile attributes

---
+++ services/user_service/src/handlers/userAttributesUpdated.js
@@ -0,0 +1,36 @@
+import pkg from 'pkg';
+const {Pool} = pkg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+export default async function handleUserUpdated(event)
+{
+    const {
+        user_id,
+        weight,
+        height,
+        age
+    } = event;
+
+    try {
+        await pool.query(
+        ` UPDATE user_attributes
+        SET
+          weight = $1,
+          height = $2,
+          age = $3
+        WHERE user_id = $4 `,
+        [
+            weight,
+            height,
+            age
+        ]
+        );
+        console.log(`User Attributes has been updated`);
+
+    } catch (error) {
+        console.error("Error updating the profile: ", error);
+    }
+}
